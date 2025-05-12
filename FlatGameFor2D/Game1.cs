612222dd786a4eb29e170e470a771f12472ddad8
Error-121 <Frederiklandgraf@gmail.com>
2025-05-12 14:44:13 +0200

    Enhance physics simulation with multiple bodies
    - Introduced `bodyList` to manage multiple `FlatBody` objects.
    - Increased camera zoom from 5 to 24 and added extents calculations.
    - Implemented random shape generation for bodies (circles and boxes).
    - Added movement logic for bodies based on keyboard input.
    - Implemented collision detection and response between bodies.
    - Updated drawing logic to render all bodies with appropriate shapes.

--- FlatGameFor2D/Game1.cs
+++ FlatGameFor2D/Game1.cs
@@ -7,6 +7,9 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Diagnostics;
 using FlatPhysicsEngineFor2D;
+using System.Collections.Generic;
+
+using FlatMath = FlatPhysicsEngineFor2D.FlatMath;
 
 namespace FlatGameFor2D
 {
@@ -18,9 +21,8 @@ namespace FlatGameFor2D
 		private Shapes shapes;
 		private Camera camera;
 
-
-		private FlatVector vectorA = new FlatVector(3f, 4f);
-
+		private List<FlatBody> bodyList;
+		private Color[] colors;
 
 		public Game1()
 		{
@@ -44,7 +46,51 @@ namespace FlatGameFor2D
 			this.sprites = new Sprites(this);
 			this.shapes = new Shapes(this);
 			this.camera = new Camera(this.screen);
-			this.camera.Zoom = 5;
+			this.camera.Zoom = 24;
+
+			this.camera.GetExtents(out float left, out float right, out float top, out float bottom);
+			
+			
+			int bodyCount = 10;
+			float padding = MathF.Abs(right - left) * 0.05f;
+
+			this.bodyList = new List<FlatBody>(bodyCount);
+			this.colors = new Color[bodyCount];
+
+			for (int i = 0; i < bodyCount; i++)
+			{
+				int type = RandomHelper.RandomInteger(0, 2);
+				type = (int)ShapeType.Circle; // TODO: remove this line to use random shape type
+
+				FlatBody body = null;
+
+				float x = RandomHelper.RandomSingle(left + padding, right - padding);
+				float y = RandomHelper.RandomSingle(top - padding, bottom + padding);
+
+
+				if (type == (int)ShapeType.Circle)
+				{
+					if (!FlatBody.CreateCircleBody(new FlatVector(x, y), 2f, 0.5f, false, 1f, out body, out string errorMessage))
+					{
+						throw new Exception();
+					}
+				}
+				else if (type == (int)ShapeType.Box)
+				{
+					if (!FlatBody.CreateBoxBody(new FlatVector(x, y), 2f, 0.5f, false, 1f, 1f, out body, out string errorMessage))
+					{
+						throw new Exception();
+					}
+				}
+				else 
+				{
+					throw new Exception("Unknown shape type");
+				}
+
+				this.bodyList.Add(body);
+				this.colors[i] = RandomHelper.RandomColor();
+
+			}
 
 			base.Initialize();
 		}
@@ -79,7 +125,42 @@ namespace FlatGameFor2D
 					this.camera.DecZoom();
 				}
 
+				float dx = 0f;
+				float dy = 0f;
+				float speed = 8f;
+
+				if (keyboard.IsKeyDown(Keys.Left)) { dx --; }
+				if (keyboard.IsKeyDown(Keys.Right)) { dx++; }
+				if (keyboard.IsKeyDown(Keys.Up)) { dy++; }
+				if (keyboard.IsKeyDown(Keys.Down)) { dy--; }
+
+				if (dx != 0f || dy != 0f)
+				{
+					FlatVector direction = FlatMath.Normalize( new FlatVector(dx, dy));
+					FlatVector velocity = direction * speed * FlatUtil.GetElapsedTimeInSeconds(gameTime);
+					this.bodyList[0].Move(velocity);
+				}
 			}
+
+			for (int i = 0; i < this.bodyList.Count -1 ; i++)
+			{
+				FlatBody bodyA = this.bodyList[i];
+
+				for (int j = i + 1; j < this.bodyList.Count; j++)
+				{
+					FlatBody bodyB = this.bodyList[j];
+
+					if (Collisions.IntersectCircles(bodyA.Position, bodyA.radius, bodyB.Position, bodyB.radius, out FlatVector normal, out float depth)) 
+					{
+						bodyA.Move(-normal * depth / 2f);
+						bodyB.Move(normal * depth / 2f);
+					}
+				}
+			}
+
+
+
+			base.Update(gameTime);
 		}
 
 		protected override void Draw(GameTime gameTime)
@@ -87,13 +168,27 @@ namespace FlatGameFor2D
 			this.screen.Set();
 			this.GraphicsDevice.Clear(new Color(50, 60, 70));
 
-			FlatVector normarlized = FlatPhysicsEngineFor2D.FlathMath.Normalize(this.vectorA);
+			
 
 			this.shapes.Begin(this.camera);
-			//this.shapes.DrawCircle(0, 0, 32, 32, Color.White);
-			this.shapes.DrawLine(Vector2.Zero, FlatConverter.ToVector2(this.vectorA), Color.Red);
-			this.shapes.DrawLine(Vector2.Zero, FlatConverter.ToVector2(normarlized), Color.Blue);
-			this.shapes.DrawCircle(Vector2.Zero, 1f, 24, Color.White);
+
+			for (int i = 0; i < bodyList.Count; i++)
+			{
+				FlatBody body = bodyList[i];
+				Vector2 position = FlatConverter.ToVector2(body.Position);
+
+				if (body.shapeType is ShapeType.Circle)
+				{
+					shapes.DrawCircleFill(position, body.radius, 26, colors[i]);
+					shapes.DrawCircle(position, body.radius, 26, Color.Red);
+				}
+				else if (body.shapeType is ShapeType.Box)
+				{
+					this.shapes.DrawBox(position, body.width, body.height, 26, Color.Blue);
+				}
+				
+			}
+
 			this.shapes.End();
 
 			this.screen.Unset();
